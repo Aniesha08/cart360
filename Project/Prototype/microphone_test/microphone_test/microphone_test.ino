8c7390bc4b00539d079681682ea8d239acb3230f
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-11-12 19:21:40 -0500

    Organized Final Project Files
    Moved proposal files to separate folder
    Uploaded prototype test code
    Uploaded prototype documentation

---
+++ Project/Prototype/microphone_test/microphone_test/microphone_test.ino
@@ -0,0 +1,42 @@
+/*
+ * Rui Santos 
+ * Complete Project Details https://randomnerdtutorials.com/guide-for-microphone-sound-sensor-with-arduino/
+*/
+
+int ledPin=13;
+int sensorPin=7;
+boolean val = 1;
+
+void setup(){
+  pinMode(sensorPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin (9600);
+}
+
+/*
+void loop (){
+  val =digitalRead(sensorPin);
+  // when the sensor detects a signal above the threshold value, LED flashes
+  if (val==HIGH) {
+    digitalWrite(ledPin, HIGH); 
+    delay(1000);
+    Serial.println (val);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
+*/
+
+void loop (){
+  val =digitalRead(sensorPin);
+  // when the sensor detects a signal above the threshold value, LED flashes
+  if (val== LOW) {
+    digitalWrite(ledPin, HIGH); 
+    delay(1000);
+    Serial.println (val);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
